6790f27a709bd88998c2429cf6a63f64ed16ddd9
grolygori789-crypto <grolygori789@gmail.com>
2026-09-08 23:19:50 +0700

    Restore HTML visual parity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.benedictinteractive.bearagnostic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.11.0-alpha11"
+        versionCode = 12
+        versionName = "0.12.0-alpha12"
     }
 
     buildFeatures {
